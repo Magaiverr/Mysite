8abedf110c99a1ede0371e0551e77ae51f786a2c
imagaiverr <Muuurilooo1@gmail.com>
2024-05-22 15:46:25 -0300

    pytest

--- mysite/tests/models/test_post.py
+++ mysite/tests/models/test_post.py
@@ -1,11 +1,13 @@
 import pytest
 
-from blog.factories import factory
+from blog.factories import PostFactory
 
 @pytest.fixture
 def post_published():
-    return factory(title='pytest with factory')
+    return PostFactory(title='pytest with factory')
+
+
 
 @pytest.mark.django_db
 def test_create_published_post(post_published):
-    assert post_published.title == 'pytest with factory'
+    assert post_published.title == 'pytest with factory' 
